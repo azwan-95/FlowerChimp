a79b943ee232274418e02e74cf1393411b8ec81d
aicvx <azwan.ismail95@gmail.com>
2025-05-14 07:52:21 +0800

    updated

--- main.py
+++ main.py
@@ -25,35 +25,52 @@ r = requests.get(testlink, headers=headers)
 
 soup = BeautifulSoup(r.content, 'lxml')
 
+# Get the name of the products
 name = soup.find('h1',class_='new-product-title').text.strip()
+
+# Get the rating out of 5
 try:
     rating = float(soup.find('span', class_='jdgm-prev-badge__stars')['data-score'])
 except:
     rating = 0
 
+# Get the percentage of rating that is verified user
 try:
     verified_rating = soup.find('div',class_='jdgm-medal__value').text.strip()
 except:
     verified_rating = 0
 
+# Get total no of buyer who provide rating
 try:
     total_rating = float(soup.find('span',class_='jdgm-prev-badge__text').text.split()[0])
 except:
     total_rating = 0
 
+# Get the availability of the delivery location
 try:
     location = soup.find('p', class_='area_availability_label_desktop').text.strip()
 except:
     location = 'none'
 
+# Get the default price, if there is no other variant of the products
 default_price = soup.find('span',itemprop='price').text.split()[1]
 
+# Get the image details
 img_tag = soup.find('img', class_='lazyload')
 if img_tag and 'src' in img_tag.attrs:
     img_url = 'https:' + img_tag['src'] if img_tag['src'].startswith('//') else img_tag['src']
 else:
     img_url = None
 
+# Get cleaned details of the bloom
+p_tag = soup.find('p')
+
+for br in p_tag.find_all('br'):
+    br.replace with('\n')
+
+details = p_tag.get_text().replace('\xa0','').strip()
+
+# Append all information in a dataframe
 data_summary = {
     'name': name,
     'rating': rating,
